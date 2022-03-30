7d8095b5c45056c36bda4651bdafa01de3f06c42
arnol.meneses <arnol.meneses@siesa.com>
2022-03-30 11:49:28 -0500

    segunda parte curso

---
+++ decuentos.js
@@ -0,0 +1,9 @@
+
+
+function calcularPrecioCOnDescuento(precio, descuento){
+    
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
+
+    return precioConDescuento;
+}
